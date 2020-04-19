b28c58b0f437fb8732082628e96521bb7f3d95e3
Howard.lee <contact-me@howardlee.cloud>
2020-04-19 11:16:06 +0800

    feat(app.js): add functionality for is send to remote successful

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ wss.on("connection", function connection(ws) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
+
+      if (client === ws && client.readyState === WebSocket.OPEN) {
+        // 處理 成功傳送到 server
+        // Notice: 補 uuid
+
+        let backToSelfData = JSON.parse(data);
+
+        backToSelfData[`isSendSuccess`] = true;
+
+        const objStr = JSON.stringify({
+          isSendSuccess: true,
+          uuid: backToSelfData[`uuid`],
+        });
+        client.send(objStr);
+      }
+
+      // TODO: 存到資料庫留底
+      // TODO: 處理資料庫中，多人已讀訊息
     });
   });
 });
